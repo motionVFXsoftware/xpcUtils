aea2e41bb1663a248c0fecfa87a2997e3e7388c5
D. Ko <machineko@protonmail.com>
2025-05-28 18:05:01 +0200

    move to binary swift-syntax for fast build

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/Concoction/swift-syntax", branch: "main"),
         .package(url: "https://github.com/machineko/SwiftyXPC", branch: "main"),
 
     ],
