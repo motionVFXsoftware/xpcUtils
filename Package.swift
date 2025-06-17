17c67af1c84e05b9f3ebe6b186c0d450e9445345
ziemowit-motionvfx <ziemowit@motionvfx.com>
2025-06-17 10:38:56 +0200

    again Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,25 +1,48 @@
 // swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "xpcUtils",
     platforms: [.macOS(.v11)],
     products: [
-        .library(name: "xpcUtils", targets: ["xpcUtils"])
+        .library(
+            name: "xpcUtils",
+            targets: ["xpcUtils"]
+        ),
+        .library(
+            name: "xpcMacros",
+            targets: ["xpcMacros"]
+        )
     ],
     dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
         .package(url: "https://github.com/machineko/SwiftyXPC", branch: "main")
     ],
     targets: [
+        .macro(
+            name: "xpcMacrosMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+        .target(
+            name: "xpcMacros", 
+            dependencies: ["xpcMacrosMacros"]
+        ),
         .target(
             name: "xpcUtils",
             dependencies: [
-                .product(name: "SwiftyXPC", package: "SwiftyXPC")
+                .product(name: "SwiftyXPC", package: "SwiftyXPC"),
+                "xpcMacros"
             ]
         ),
         .testTarget(
             name: "xpcUtilsTests",
-            dependencies: ["xpcUtils"]
+            dependencies: ["xpcUtils", "xpcMacros"]
         )
     ]
 )
