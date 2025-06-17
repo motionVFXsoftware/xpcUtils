2e2e970efdce121d97a07d7d1d3ecaf553296598
ziemowit-motionvfx <ziemowit@motionvfx.com>
2025-06-17 10:09:51 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
 // swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 import CompilerPluginSupport
 
@@ -12,10 +14,14 @@ let package = Package(
         .library(
             name: "xpcMacros",
             targets: ["xpcMacros"]
+        ),
+        .executable(
+            name: "xpcMacrosMacros",
+            targets: ["xpcMacrosMacros"]
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", branch: "main"),
         .package(url: "https://github.com/machineko/SwiftyXPC", branch: "main"),
     ],
     targets: [
@@ -24,26 +30,13 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ],
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
             ]
         ),
-        .target(
-            name: "xpcMacros", 
-            dependencies: ["xpcMacrosMacros"],
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
-            ]
-        ),
+        .target(name: "xpcMacros", dependencies: ["xpcMacrosMacros"]),
         .target(
             name: "xpcUtils",
             dependencies: [
-                .product(name: "SwiftyXPC", package: "SwiftyXPC"), 
-                "xpcMacros"
-            ],
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
+                .product(name: "SwiftyXPC", package: "SwiftyXPC"), "xpcMacros"
             ]
         ),
         .testTarget(
