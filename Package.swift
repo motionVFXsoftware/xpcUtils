4dc019a9b675911c34e8cf06480d7f5d22280a58
ziemowit-motionvfx <ziemowit@motionvfx.com>
2025-06-17 10:37:34 +0200

    removed macros for test Package.swift

--- Package.swift
+++ Package.swift
@@ -1,50 +1,25 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-import CompilerPluginSupport
 
 let package = Package(
     name: "xpcUtils",
     platforms: [.macOS(.v11)],
     products: [
-        .library(
-            name: "xpcUtils",
-            targets: ["xpcUtils"]
-        ),
-        .library(
-            name: "xpcMacros",
-            targets: ["xpcMacros"]
-        )
+        .library(name: "xpcUtils", targets: ["xpcUtils"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
         .package(url: "https://github.com/machineko/SwiftyXPC", branch: "main")
     ],
     targets: [
-        .macro(
-            name: "xpcMacrosMacros",
-            dependencies: [
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ],
-            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
-        ),
-        .target(
-            name: "xpcMacros", 
-            dependencies: ["xpcMacrosMacros"],
-            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
-        ),
         .target(
             name: "xpcUtils",
             dependencies: [
-                .product(name: "SwiftyXPC", package: "SwiftyXPC"),
-                "xpcMacros"
+                .product(name: "SwiftyXPC", package: "SwiftyXPC")
             ]
         ),
         .testTarget(
             name: "xpcUtilsTests",
-            dependencies: ["xpcUtils", "xpcMacros"]
+            dependencies: ["xpcUtils"]
         )
     ]
 )
